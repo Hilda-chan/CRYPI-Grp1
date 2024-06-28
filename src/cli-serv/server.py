3f5a4ce3dc968fd76be0b100bb20b53c007e0db7
Oscar Mrad <oscar.mrad@epita.fr>
2024-06-28 18:45:33 +0200

    server

--- src/cli-serv/server.py
+++ src/cli-serv/server.py
@@ -1,5 +1,13 @@
 import socket
 import pickle
+import os
+import sys
+import Pyfhel
+import numpy as np
+import cv2
+sys.path.append('../')
+from prototype import readImg
+import face_recognition
 
 def start_server(host='localhost', port=65432):
     # Create a socket object
@@ -30,14 +38,50 @@ def start_server(host='localhost', port=65432):
 
 
                 print(f'Received data: Firstname={firstname}, Lastname={lastname}, Key={key}, Vector={vector}')
-                
         
-        
-                response = f'Welcome {firstname} {lastname}'
+                # Construct the directory path
+                directory_path = f'../../data/LFW/{firstname}_{lastname}'
+
+                # Check if the directory exists
+                if os.path.exists(directory_path):
+                    # List images in the directory
+                    images = os.listdir(directory_path)
+                    # Look for the specific image
+                    image_path = None
+                    for image in images:
+                        if image.startswith(f'{firstname}_{lastname}_0001.jp'):
+                            image_path = os.path.join(directory_path, image)
+                            img = readImg(image_path)
+                            HE = Pyfhel.Pyfhel()
+                            ckks_params = {
+                                'scheme': 'CKKS',   # can also be 'ckks'
+                                'n': 2**14,         # Polynomial modulus degree. For CKKS, n/2 values can be
+                                                    #  encoded in a single ciphertext.
+                                                    #  Typ. 2^D for D in [10, 15]
+                                'scale': 2**30,     # All the encodings will use it for float->fixed point
+                                                    #  conversion: x_fix = round(x_float * scale)
+                                                    #  You can use this as default scale or use a different
+                                                    #  scale on each operation (set in HE.encryptFrac)
+                                'qi_sizes': [60, 30, 30, 30, 60] # Number of bits of each prime in the chain.
+                                                    # Intermediate values should be  close to log2(scale)
+                                                    # for each operation, to have small rounding errors.
+                            }
+                            HE.contextGen(**ckks_params)  # Generate context for ckks scheme
+                            HE.keyGen()
+                            v = getFaceVectors(img)
+
+                            break
+                    if image_path:
+                        response = f'Image found for {firstname} {lastname}.'
+
+                    else:
+                        response = f'Directory found but not image for {firstname} {lastname}.'
+
+                else:
+                    response = f'Image not found for {firstname} {lastname}'        
                 
                 # Send the response back to the client
                 conn.sendall(response.encode('utf-8'))
 
 if __name__ == "__main__":
     start_server()
-
